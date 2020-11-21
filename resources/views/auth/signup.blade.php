3a20f78df47bce9b031531ef631e063a6e190201
Pedro Sérgio <www.psro@gmail.com>
2020-11-21 14:59:12 -0300

    sign up form updated

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -3,33 +3,31 @@
 @section('content')
 
 <div class="container">
-    <form method="post" action="/signup">
+    <form method="post" action="/signup" class="d-flex justify-content-center">
         @csrf
         <div class="p-centered" style="width: 100vh; max-width: 90%">
-                <h1 class="text-center text-primary my-2">Sign up</h1>
-                <p class="text-center">
-                    Join us by creating an account!
-                </p>
+                <h1 class="text-secondary my-2">Sign up</h1>
+                <h4 class="text-muted mb-4">Join us by creating an account!</h4>
                 
                 <div class="form-group">
-                    <label class="form-label  {{$errors->has('username')? 'text-error' : ''}}" for="username">Username</label>
+                    <label class="form-label  {{$errors->has('username')? 'is-invalid' : ''}}" for="username">Username:</label>
                     <input 
-                        class="form-input  {{$errors->has('username')? 'is-error' : ''}}" 
+                        class="form-control  {{$errors->has('username')? 'is-invalid' : ''}}" 
                         type="text" 
-                        id="name" 
+                        id="username" 
                         name="username"
                         placeholder="Enter your username"
                         value="{{ Request::old('username') ?? '' }}"
                     >
-                    @if ($errors->has('username'))
-                        <span class="text-error">{{ $errors->first('username') }}</span>
+                    @if ($errors->has('email'))
+                        <small class="text-danger">{{ $errors->first('email') }}</small>
                     @endif
                 </div>
 
                 <div class="form-group">
-                    <label class="form-label  {{$errors->has('email')? 'text-error' : ''}}" for="email">E-mail</label>
+                    <label class="form-label  {{$errors->has('email')? 'is-invalid' : ''}}" for="email">E-mail:</label>
                     <input 
-                        class="form-input  {{$errors->has('email')? 'is-error' : ''}}" 
+                        class="form-control  {{$errors->has('email')? 'is-invalid' : ''}}" 
                         type="email" 
                         id="email" 
                         name="email"
@@ -37,22 +35,21 @@
                         value="{{ Request::old('email') ?? '' }}"
                     >
                     @if ($errors->has('email'))
-                        <span class="text-error">{{ $errors->first('email') }}</span>
+                        <small class="text-danger">{{ $errors->first('email') }}</small>
                     @endif
                 </div>
 
-
                 <div class="form-group">
-                    <label class="form-label  {{$errors->has('password')? 'text-error' : ''}}" for="password">Password</label>
-                    <input
-                        class="form-input  {{$errors->has('password')? 'is-error' : ''}}" 
+                    <label class="form-label  {{$errors->has('password')? 'is-invalid' : ''}}" for="password">Password:</label>
+                    <input 
+                        class="form-control  {{$errors->has('email')? 'is-invalid' : ''}}" 
                         type="password" 
                         id="password" 
                         name="password"
                         placeholder="Enter your password"
                     >
-                    @if ($errors->has('password'))
-                        <span class="text-error">{{ $errors->first('password') }}</span>
+                    @if ($errors->has('email'))
+                        <small class="text-danger">{{ $errors->first('password') }}</small>
                     @endif
                 </div>
 

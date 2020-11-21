f988c7e38fc175ddf777d6e230670e40b3559857
Pedro Sérgio <www.psro@gmail.com>
2020-11-21 15:23:57 -0300

    toast alert updated

--- resources/views/templates/components/alert.blade.php
+++ resources/views/templates/components/alert.blade.php
@@ -1,9 +1,32 @@
 @if (Session::has('info'))
-    <div class="toast" style="border-radius:0">
-        {{ Session::get('info') }}
-        <button 
-            onclick="this.parentElement.style.display='none';"
-            class="btn btn-clear float-right"
-        ></button>
+
+    <div 
+        class="toast" 
+        style="position: absolute; width: 320px; left: 2%; top: 10%" 
+        role="alert" 
+        aria-live="assertive" 
+        aria-atomic="true"
+    >
+        <div class="toast-header">
+            <i class="icofont-bell"></i>
+            <strong class="mr-auto">Yay!</strong>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+
+        <div class="toast-body">
+            {{ Session::get('info') }}
+        </div>
+
     </div>
+
+    <script>
+        $('.toast').toast({
+            delay: 4000,
+            animation: true,
+        })
+        $('.toast').toast('show')
+    </script>
+
 @endif

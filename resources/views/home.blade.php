9bcf1c130b61286b5d10d5a777f6aaf69ac83cdf
Pedro Sérgio <www.psro@gmail.com>
2020-11-21 14:25:44 -0300

    home updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,21 @@
 @extends('templates.default')
 
 @section('content')
+    <img 
+        src="https://source.unsplash.com/1600x900/?social" 
+        style="height: 30vh; width: 100vw; object-fit: cover"
+        class="img-fluid" 
+    >
     
-    <div class="hero container bg-primary">
-        <div class="hero-body">
-            <h1>Welcome to Yay</h1>
-            <p>
-                Share experiences and thoughts with people that <br>
-                don't really know you and also don't care <br>
-                will be fun! :^D 
-            </p>
-        </div>
+    <div class="jumbotron" style="height: 56vh">
+        <h1 class="display-4">Welcome to Yay</h1>
+        <p class="lead">Share experiences and thoughts with people that <br>
+                        don't really know you and also don't care
+        </p>
+        <hr class="my-4">
+        
+        <p>It  will be fun! :^D</p>
+        
+        <a class="btn btn-primary btn-lg" href="/signup" role="button">Sign up</a>
     </div>
 @stop

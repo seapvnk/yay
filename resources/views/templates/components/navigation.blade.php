e4a17b032017c9c595b2414df7ee81a42105db57
Pedro Sérgio <www.psro@gmail.com>
2020-11-21 14:02:39 -0300

    moved stylesheets to boostrap

--- resources/views/templates/components/navigation.blade.php
+++ resources/views/templates/components/navigation.blade.php
@@ -1,49 +1,50 @@
-<header class="navbar bg-primary container" style="padding: .5rem">
-  <section class="navbar-section">
-    <a href="/" class="navbar-brand mr-2 text-light text-bold">yay!</a>
-    
-    @if (Auth::check())
-      <div style="margin-left: 2rem">
-        <a href="/home" class="mx-1 text-light">Timeline</a>
-        <a href="/friends" class="mx-2 text-light">Friends</a>
-      </div>
+<header>
+  <nav class="navbar navbar-expand-lg navbar-primary bg-primary">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon d-inline-flex justify-content-center align-items-center">
+        <i class="text-light icofont-navigation-menu"></i>
+        </span>
+    </button>
 
-      <form action="/search">
-        @csrf
-        <div 
-          class="input-group input-inline mx-2"
-        >
-            <input class="form-input" name="query" type="text" placeholder="search">
-            <button
-              class="mx-1 btn input-group-btn" 
-            >
-              Search <i class="icofont-search"></i>
-            </button>
-        </div>
-      </form>
-    @endif
-  </section>
-
-  @if (!Auth::check())
-    <section class="navbar-section">
-        <a href="/signup" class="mx-1 text-light">Sign up</a>
-        <a href="/signin" class="mx-1 text-light">Sign in</a>
-    </section>
-  @else
-      <section class="navbar-section">
-
-
-        <a href="/profile/edit" class="mx-1 text-light">Update profile</a>
-        <a href="/signout" class="mx-1 text-light">Sign out</a>
-        
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li>
+          <a class="navbar-brand" href="/">yay!</a>
+        </li>
+        @if (Auth::check())
+          <li class="nav-item">
+            <a class="nav-link" href="/home">Timeline</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/friends">Friends</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/profile/edit">Edit profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/signout">Sign out</a>
+          </li>
+        @else
+          <li class="nav-item">
+            <a class="nav-link" href="/signup">Sign up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/signin">Sign in</a>
+          </li>
+        @endif
+      </ul>
+      @if (Auth::check())
+        <form action="/search" class="form-inline my-2 my-lg-0" >
+          @csrf
+          <input class="form-control mr-sm-2" name="search" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search <i class="icofont-search"></i></button>
+        </form>
         <a 
           href="/user/{{ Auth::user()->username }}" 
-          class="btn btn-link text-light text-bold btn-lg"
-        >
-          <img class="avatar" style="background-color: #0004" src="{{ Auth::user()->getAvatarURL() }}" alt="">
-          {{ "@" . Auth::user()->getNameOrUsername() }}
+          class="btn btn-link text-light text-bold btn-lg">
+          <img class="rounded-circle" style="background-color: #0004; width: 64px" src="{{ Auth::user()->getAvatarURL() }}" alt="">
         </a>
-      </section>
-      
-  @endif
+      @endif
+    </div>
+  </nav>
 </header>
--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -5,7 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://allyoucan.cloud/cdn/icofont/1.0.1/icofont.css">
-    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
+    
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://bootswatch.com/4/slate/bootstrap.min.css">
+
+    <!-- jQuery and JS bundle w/ Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
     
     <title>yay!</title>
 </head>

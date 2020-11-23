b4af477c277d194d4bd72bef3bc34b2c40961d26
Pedro Sérgio <www.psro@gmail.com>
2020-11-22 21:59:29 -0300

    timeline page updated

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -1,63 +1,57 @@
 @extends('templates.default')
 
 @section('content')
-
-<style>
-    .row {
-        margin: auto;
-        width: 60%;
-    }
-
-    @media screen and (max-width: 768px) {
-        .row {
-            width: 100%;
-        }   
-    }
-
-</style>
     
     <div class="container">
-        <div class="row">
-            <form action="/status" method="post">
-                <div class="form-group">
-                    <label class="form-label" for="status">Status</label>
-                    <textarea class="form-input" style="resize: none" name="status" id="status" placeholder="What's up {{ Auth::user()->getNameOrUsername() }}" rows="3"></textarea>
-                </div>
-                <input type="hidden" name="_token" value="{{ Session::token() }}">
-                @if ($errors->has('status'))
-                    <div>
-                        <span class="text-error">
-                            {{ $errors->first('status') }}
-                        </span>
-                    </div>
-                @endif
-                <button
-                    role="submit" 
-                    class="btn btn-lg {{ $errors->has('status')? 'btn-error' : '' }}"
-                >
-                    Update status
-                </button>
-            </form>
+        <form action="/status" method="post">
 
-            <div class="divider"><div>
+            <div class="form-group mt-4">
+                <textarea 
+                    class="form-control {{ $errors->has('status') ? 'is-invalid' : '' }}" 
+                    style="resize: none" 
+                    name="status" 
+                    id="status" 
+                    placeholder="What's up {{ Auth::user()->getNameOrUsername() }}" 
+                    rows="3"></textarea>
+            </div>
 
-            @if (!$statuses->count())
-                <div class="empty">
-                    <div class="empty-icon">
-                        <i style="font-size: 16vh" class="icofont-users"></i>
-                    </div>
-                    <p class="empty-title h5">Nothing to see :p</p>
-                    <p class="empty-subtitle">
-                        There's nothing in your timeline, yet.
-                    </p>
+            <input type="hidden" name="_token" value="{{ Session::token() }}">
+            @if ($errors->has('status'))
+                <div>
+                    <span class="text-danger">
+                        {{ $errors->first('status') }}
+                    </span>
                 </div>
-            @else
-                @foreach ($statuses as $status)
-                    @include('timeline.components.status')
-                @endforeach
             @endif
 
-            {{ $statuses->render() }}
-        </div>
+            <button
+                role="submit" 
+                class="btn float-right btn-info {{ $errors->has('status')? 'btn-danger' : '' }}"
+            >
+                Update status
+            </button>
+        </form>
+        <div class="clearfix"></div>
+
+        <hr>
+
+        @if (!$statuses->count())
+            <div class="d-flex flex-column align-items-center justify-content-center">
+                <div class="empty-icon">
+                    <i style="font-size: 16vh" class="icofont-users"></i>
+                </div>
+                <p class="empty-title h5">Nothing to see :p</p>
+                <p class="empty-subtitle">
+                    There's nothing in your timeline, yet.
+                </p>
+            </div>
+        @else
+            @foreach ($statuses as $status)
+                @include('timeline.components.status')
+            @endforeach
+        @endif
+
+        {{ $statuses->render() }}
+
     </div>
 @stop

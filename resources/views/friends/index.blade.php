a007d04381eb15abfca194308074417fcbd51098
Pedro Sérgio <www.psro@gmail.com>
2020-11-24 20:59:11 -0300

    friend requests layout updated

--- resources/views/friends/index.blade.php
+++ resources/views/friends/index.blade.php
@@ -1,62 +1,37 @@
 @extends('templates.default')
 
-<style>
-
-    .friends-container {
-        width: 75% !important;
-        margin: auto;
-        display: flex;
-        align-items: center;
-        margin-top: 5vh;
-        flex-flow: column;
-    }
-
-    .col {
-        width: 100%;
-    }
-
-</style>
-
 @section('content')
     
-    <div class="container friends-container">
-        <div class="col">
-            <h4 class="text-center">Friend requests</h4>
+    <div class="container pt-4">
+        <div class="row p-4 d-flex align-items-center justify-content-center">
+            <div class="col d-flex flex-column justify-content-center align-items-center">
+                <h4 class="text-center">Friend requests</h4>
 
-            @if (!$requests->count())
-            <div class="empty">
-                <div class="empty-icon">
-                    <i style="font-size: 16vh" class="icofont-users"></i>
-                </div>
-                <p class="empty-title h5">No friend requests</p>
-                <p class="empty-subtitle">
-                    You don't have any friend request
-                </p>
+                @if (!$requests->count())
+                    @include('templates.components.empty', [
+                        'title' => 'No friend requests!',
+                        'message' => 'You have no friend requests'
+                    ])
+                @else
+                    @foreach ($requests as $user)
+                        @include('user.components.request_block')
+                    @endforeach
+                @endif
             </div>
-            @else
-                @foreach ($requests as $user)
-                    @include('user.components.request_block')
-                @endforeach
-            @endif
-        </div>
-        <div class="col">
-            <h4 class="text-center">Your friends</h4>
+            <div class="col d-flex flex-column justify-content-center align-items-center">
+                <h4 class="text-center">Your friends</h4>
 
-            @if (!$friends->count())
-            <div class="empty">
-                <div class="empty-icon">
-                    <i style="font-size: 16vh" class="icofont-users"></i>
-                </div>
-                <p class="empty-title h5">No friends</p>
-                <p class="empty-subtitle">
-                    You don't have any friend
-                </p>
+                @if (!$friends->count())
+                    @include('templates.components.empty', [
+                        'title' => 'No friends!',
+                        'message' => 'You have no friends'
+                    ])
+                @else
+                    @foreach ($friends as $user)
+                        @include('user.components.block')
+                    @endforeach
+                @endif
             </div>
-            @else
-                @foreach ($friends as $user)
-                    @include('user.components.block')
-                @endforeach
-            @endif
         </div>
     </div>
     

dcd672b38a74ab89906e2cc77f3820fde81e7422
Pedro Sérgio <www.psro@gmail.com>
2020-12-29 18:21:35 -0300

    empty state message updated

--- resources/views/templates/components/empty.blade.php
+++ resources/views/templates/components/empty.blade.php
@@ -1,5 +1,5 @@
-<div class="p-4 shadow-sm  text-muted d-flex flex-column align-items-center justify-content-center">
+<div class="text-muted d-flex flex-column align-items-center justify-content-center">
     
-    <h4 class="display-4">{{ $title }}</h4>
+    <h4>{{ $title }}</h4>
     <p>{{ $message }}</p>
 </div>
